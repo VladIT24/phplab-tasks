3642daec3a4fcb18581c4f9ed7cafd621bcdea86
Vlad <vladdrachenko@ukr.net>
2021-02-12 10:19:12 +0200

    a few changes in 01-basics.php

--- src/01-basics.php
+++ src/01-basics.php
@@ -55,11 +55,11 @@ function isLeapYear(int $year)
     }
 
 
-    if ($year % 4 == 0) {
+    if ($year % 4 == 0 && $year % 100 != 0) {
         return true;
     }
 
-    if ($year % 100 == 0 && $year % 400 == 0) {
+    if ($year % 400 == 0) {
         return true;
     }
 
